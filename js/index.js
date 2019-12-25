5446d6bd79f3f416d2c8a46910c4aeb96768a9ab
maxonchil <maxonchil@gmail.com>
2019-12-25 21:01:37 +0200

    Fix first problem with JS(reistation form) *Delete global variable and now hold 'users_counter' on localeStorage, becouse of after page reload, variable is redeclared *Add regexp to verify that the user entered a valid email address *Change a position and animation of aside submenu, now its position:absolte *Change font-size for search field,to  make it more responsive *Change animation time of long sumbenu-1(in header) to make animation more smooth

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
-let users_counter = 0;
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementsByClassName("create-accountForm__submit")[0].onclick = () => {
-        let reg_userName = document.querySelectorAll("[name='user-name']")[0].value;
-        let reg_userEmail = document.querySelectorAll("[name='user-email']")[0].value;
-        let reg_userPassword = document.querySelectorAll("[name='user-password']")[0].value;
-        let reg_userPassword2 = document.querySelectorAll("[name='user-password2']")[0].value;
+        let reg_userName = document.querySelectorAll("[name='user-name']")[0].value,
+        reg_userEmail = document.querySelectorAll("[name='user-email']")[0].value,
+        reg_userPassword = document.querySelectorAll("[name='user-password']")[0].value,
+        reg_userPassword2 = document.querySelectorAll("[name='user-password2']")[0].value,
+        email_regexp = /.+@.+\..+/;
         if (reg_userPassword !== reg_userPassword2) {
             alert("Passwords do not match!");
             return;
         } else {
             reg_userPassword = reg_userPassword2;
+        }
+        if(email_regexp.test(reg_userEmail)) {
+            reg_userEmail = reg_userEmail;
+        }else {
+            alert("Please use a correct email");
+            return;
         }
 
         let reg_userData = {
@@ -19,19 +25,11 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         if (localStorage.getItem("id0") === null) {
-            localStorage.setItem(`id${users_counter}`, JSON.stringify(reg_userData));
-            users_counter++;
-            console.log(users_counter);
+            localStorage.setItem('users_counter', 1);
+            localStorage.setItem(`id0`, JSON.stringify(reg_userData));
             return;
         } else {
-            for (let i = 0; i < users_counter; i++) {
-                if (JSON.parse(localStorage.getItem(`id${i}`)).user_email === reg_userEmail) {
-                    alert("A user with this email is alredy registered")
-                    return;
-                } else {
-                    reg_userEmail = reg_userEmail;
-                }
-            }
+            let users_counter = +localStorage.getItem('users_counter');
             for (let i = 0; i < users_counter; i++) {
                 if (JSON.parse(localStorage.getItem(`id${i}`)).user_name === reg_userName) {
                     alert("A user with this name is already registered ")
@@ -40,10 +38,21 @@ document.addEventListener("DOMContentLoaded", () => {
                     reg_userName = reg_userName;
                 }
             }
+            for (let i = 0; i < users_counter; i++) {
+                if (JSON.parse(localStorage.getItem(`id${i}`)).user_email === reg_userEmail) {
+                    alert("A user with this email is alredy registered")
+                    return;
+                } else {
+                    reg_userEmail = reg_userEmail;
+                }
+            }
+
+            localStorage.setItem(`id${users_counter}`, JSON.stringify(reg_userData));
+            users_counter++;
+            localStorage.setItem('users_counter', users_counter);
         }
 
-        localStorage.setItem(`id${users_counter}`, JSON.stringify(reg_userData));
-        users_counter++;
+
     }
 
 
