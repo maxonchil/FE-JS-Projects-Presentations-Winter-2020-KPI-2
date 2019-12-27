002094036fdc6b9cdd6a233e9727656a396a17dc
maxonchil <maxonchil@gmail.com>
2019-12-27 17:59:33 +0200

    *Change method of using svg icons, make HandMake font-style for it *Change a position of aside menu elemets, make it vertical align center, by absolute position on diferent devices *Find and fix bug with box shadow on slider controls *Change width of client pagination layout, now its correctly displayed on 500 - 2k screen width devices

--- js/index.js
+++ js/index.js
@@ -4,26 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
             reg_userEmail = document.querySelectorAll("[name='user-email']")[0].value,
             reg_userPassword = document.querySelectorAll("[name='user-password']")[0].value,
             reg_userPassword2 = document.querySelectorAll("[name='user-password2']")[0].value,
-            email_regexp = /\w+@\w+\.\w+/i;
+            email_regexp = /\w+@\w+\.\w+/i,
+            reg_userData = 
+            {
+                "user_name": reg_userName,
+                "user_email": reg_userEmail,
+                "user_password": reg_userPassword
+            };
+            
         if (reg_userPassword !== reg_userPassword2) {
             alert("Passwords do not match!");
             return;
-        } else {
-            reg_userPassword = reg_userPassword2;
         }
-        if (email_regexp.test(reg_userEmail)) {
-            reg_userEmail = reg_userEmail;
-        } else {
+        if (!email_regexp.test(reg_userEmail)) {
             alert("Please use a correct email");
             return;
-        }
-
-        let reg_userData = {
-            "user_name": reg_userName,
-            "user_email": reg_userEmail,
-            "user_password": reg_userPassword
-        }
-
+        } 
         if (localStorage.getItem("id0") === null) {
             localStorage.setItem('users_counter', 1);
             localStorage.setItem(`id0`, JSON.stringify(reg_userData));
@@ -34,17 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (JSON.parse(localStorage.getItem(`id${i}`)).user_name === reg_userName) {
                     alert("A user with this name is already registered ")
                     return;
-                } else {
-                    reg_userName = reg_userName;
-                }
+                } 
             }
             for (let i = 0; i < users_counter; i++) {
                 if (JSON.parse(localStorage.getItem(`id${i}`)).user_email === reg_userEmail) {
                     alert("A user with this email is alredy registered")
                     return;
-                } else {
-                    reg_userEmail = reg_userEmail;
-                }
+                } 
             }
 
             localStorage.setItem(`id${users_counter}`, JSON.stringify(reg_userData));
