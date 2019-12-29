bb5c2eb1f1758d683b88d1eaf44c9df76a680064
maxonchil <maxonchil@gmail.com>
2019-12-29 19:14:38 +0200

    *Kill cookie if page reloaded *Only one error in process registation on sign in can be showed *If registration or sign in was successful, then inputs value clear and errors area also clear

--- js/index.js
+++ js/index.js
@@ -1,24 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
     let users_base = JSON.parse(localStorage.getItem('users_base'));
+    if (/^logIn/.test(document.cookie)) {
+        document.cookie = "logIn=true; max-age=0";
+    }
     document.getElementsByClassName("create-accountForm__submit")[0].onclick = () => {
         let reg_userName = document.querySelector("[name='user-name']").value,
             reg_userEmail = document.querySelector("[name='user-email']").value,
             reg_userPassword = document.querySelector("[name='user-password']").value,
             reg_userPassword2 = document.querySelector("[name='user-password2']").value,
+            reg_errorEmail = document.querySelector(".create-accountForm__errorEmail"),
+            reg_erroName = document.querySelector(".create-accountForm__errorName"),
+            reg_errorPas = document.querySelector(".create-accountForm__errorPas"),
             reg_userData = {
                 "user_name": reg_userName,
                 "user_email": reg_userEmail,
                 "user_password": reg_userPassword
             };
         if (!/\w+@\w+\.\w+/i.test(reg_userEmail)) {
-            document.querySelector(".create-accountForm__errorEmail").innerHTML = "Please use a correct email";
+            reg_errorEmail.innerHTML = "Please use a correct email";
             return;
         }
         if (reg_userPassword.length < 5) {
-            document.querySelector(".create-accountForm__errorPas").innerHTML = "Password must be longer than 5 characters";
+            reg_errorPas.innerHTML = "Password must be longer than 5 characters";
+            reg_errorEmail.innerHTML = "";
             return;
         } else if (reg_userPassword !== reg_userPassword2) {
-            document.querySelector(".create-accountForm__errorPas").innerHTML = "Passwords do not match";
+            reg_errorPas.innerHTML = "Passwords do not match";
+            reg_errorEmail.innerHTML = "";
             return;
         }
 
@@ -46,39 +54,56 @@ document.addEventListener("DOMContentLoaded", () => {
             })(lsKey, lsValue)
             document.querySelector("[name='create-acctountForm']").style.display = "none";
             document.querySelector(".create-accountForm__success").style.display = "block";
+            document.querySelectorAll("input[name^='user-']").forEach(e => {
+                e.value = "";
+            })
+            reg_errorEmail.innerHTML = "";
+            reg_erroName.innerHTML = "";
+            reg_errorPas.innerHTML = "";
             return;
+
         } else {
             let users_counter = users_base[0].users_counter;
             //start loop from i = 1 becouse of inde 0 is a users counter
             for (let i = 1; i <= users_counter; i++) {
                 if (users_base[i].user_name === reg_userName) {
-                    document.querySelector(".create-accountForm__errorName").innerHTML = "Nickname is already taken";
+                    reg_erroName.innerHTML = "Nickname is already taken";
+                    reg_errorPas.innerHTML = "";
+                    reg_errorEmail.innerHTML = "";
                     return;
                 }
             }
             for (let i = 1; i <= users_counter; i++) {
                 if (users_base[i].user_email === reg_userEmail) {
-                    document.querySelector(".create-accountForm__errorEmail").innerHTML = "This email is alredy used";
+                    reg_errorEmail.innerHTML = "This email is alredy used";
+                    reg_erroName.innerHTML = "";
+                    reg_errorPas.innerHTML = "";
                     return;
                 }
             }
 
             users_base.push(reg_userData);
             users_base[0].users_counter++
-            let lsKey ="users_base",
-            lsValue = JSON.stringify(users_base);
-                ((key, item) => {
-                    try {
+            let lsKey = "users_base",
+                lsValue = JSON.stringify(users_base);
+            ((key, item) => {
+                try {
+                    localStorage.setItem(key, item);
+                } catch (error) {
+                    if (error.name === "QUOTA_EXCEEDED_ERR" || error.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
+                        localStorage.clear();
                         localStorage.setItem(key, item);
-                    } catch (error) {
-                        if (error.name === "QUOTA_EXCEEDED_ERR" || error.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
-                            localStorage.clear();
-                            localStorage.setItem(key, item);
-                        }
                     }
-                })(lsKey, lsValue)
+                }
+            })(lsKey, lsValue)
             document.querySelector("[name='create-acctountForm']").style.display = "none";
             document.querySelector(".create-accountForm__success").style.display = "block";
+            document.querySelectorAll("input[name^='user-']").forEach(e => {
+                e.value = "";
+            })
+            reg_errorEmail.innerHTML = "";
+            reg_erroName.innerHTML = "";
+            reg_errorPas.innerHTML.innerHTML = "";
         }
 
 
@@ -92,10 +117,10 @@ document.addEventListener("DOMContentLoaded", () => {
             logOut_btn = document.querySelector(".sign-inForm__logOut"),
             signIN_success = document.querySelector(".sign-inForm__success"),
             users_counter = users_base[0].users_counter,
-
             user_index = 0,
             check_log = false,
             check_pas = true;
+
         for (let i = 1; i <= users_counter; i++) {
             if (signIn_log === users_base[i].user_name) {
                 check_log = true;
@@ -104,6 +129,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
         signIn_pas === users_base[user_index].user_password ? check_pas = true : check_pas = false;
+
         if (check_log === true && check_pas === true) {
             document.getElementById('rc1').checked = 'checked';
             createAcc_btn.style.display = "none";
@@ -111,21 +137,27 @@ document.addEventListener("DOMContentLoaded", () => {
             logOut_btn.style.display = "block";
             signIN_success.innerHTML = `Hi, ${users_base[user_index].user_name}`;
             signIN_success.style.display = "block";
-            document.cookie = `username = ${users_base[user_index].user_name}`;
+            document.cookie = "logIn=true";
+            document.querySelectorAll("input[name^='sign-in']").forEach(e => {
+                e.value = "";
+            })
+            document.querySelector(".sign-inForm__error").innerHTML = "";
 
 
             logOut_btn.onclick = () => {
-                document.cookie = `username = ${users_base[user_index].user_name}; max-age=0`;
+                document.cookie = "logIn=true; max-age=0";
                 logOut_btn.style.display = "none";
                 signIN_success.style.display = "none";
                 createAcc_btn.style.display = "block";
                 signIn_btn.style.display = "block";
                 document.querySelector('.create-accountForm__success').style.display = "none";
                 document.querySelector("[name='create-acctountForm']").style.display = "block";
-
             }
 
         } else {
+            document.querySelectorAll("input[name^='sign-in']").forEach(e => {
+                e.value = "";
+            })
             document.querySelector(".sign-inForm__error").innerHTML = "Wrong email or password";
         }
 
@@ -133,11 +165,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     document.querySelector(".header__upload").onclick = (event) => {
-        if (!/^username/.test(document.cookie)) {
-            alert("Please login first or create acount!")
-            event.preventDefault();
-            return;
-        } else {
+        if (/^logIn/.test(document.cookie)) {
             let input_upload = document.getElementById("upload");
             input_upload.onchange = () => {
                 for (i = 0; i < input_upload.files.length; i++) {
@@ -145,7 +173,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             }
 
-
+        } else {
+            alert("Please login first or create acount!")
+            event.preventDefault();
+            return;
         }
     }
 })
