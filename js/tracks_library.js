300c1d77b8b64b03c97fdcd31968318172d08af7
maxonchil <maxonchil@gmail.com>
2020-01-13 15:22:15 +0200

    *Renamed all 'tracks' words into a 'soundtrack' *Changed pagination controls(functionality) *Added button 'first page' and 'last page' into a pagination controls (not working yet)

--- js/tracks_library.js
+++ js/tracks_library.js
@@ -1,90 +1,98 @@
+/*Use AJAX require to write data to localeStorage from JSON file and on readyState = 
+4 activate a function for using this soundtracks on website*/
 let xhr2 = new XMLHttpRequest();
 xhr2.onreadystatechange = function () {
     if (xhr2.readyState == 4 && xhr2.status == 200) {
         try {
-            localStorage.setItem("tracks_base", xhr2.responseText);
-            useTracks();
+            localStorage.setItem("soundtracks_base", xhr2.responseText);
+            useSoundTracks();
         } catch (error) {
             if (error.name === "QUOTA_EXCEEDED_ERR" || error.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
                 localStorage.clear();
-                localStorage.setItem("tracks_base", xhr2.responseText);
+                localStorage.setItem("soundtracks_base", xhr2.responseText);
             }
         }
 
     }
 }
 
-xhr2.open('GET', '../tracks_base.json');
+xhr2.open('GET', '../soundtracks_base.json');
 xhr2.send();
 
-function useTracks() {
-
-    let tracks_base = JSON.parse(localStorage.getItem("tracks_base"));
-
+function useSoundTracks() {
+    //Create an array with all soundtracks(take it from localeStorage)
+    let soundtracks_base = JSON.parse(localStorage.getItem("soundtracks_base")),
+        soundtracks_array = [];
+    //Selected controls, audio elements and their 'p' tag for show a soundtrack name
     let library_titles = document.querySelectorAll(".library__main-title"),
         library_audio = document.querySelectorAll(".library__main-audio"),
         library_controls = document.querySelectorAll(".library__bottom-controlsItem");
 
+    // Bring songs and their names to the page
     for (let i = 0; i < 10; i++) {
-        library_titles[i].innerHTML = tracks_base[i].trackName;
-        library_audio[i].setAttribute("src", tracks_base[i].src);
+        library_titles[i].innerHTML = soundtracks_base[i].trackName;
+        library_audio[i].setAttribute("src", soundtracks_base[i].src);
     }
+    //Added -active class to set up defoult chosed controler
+    library_controls[0].className += "-active";
 
-        for (let i = 0; i < library_controls.length; i++) {
+    //Added event on all library controls (pagination functionality)
+    for (let i = 0; i < library_controls.length; i++) {
 
-            library_controls[i].onclick = (e) => {
+        library_controls[i].onclick = (e) => {
 
-                let curent_page = +e.target.innerText,
-                    tracks_count = library_controls[i].innerText + 0,
-                    tracks_array = [];
+            let curent_page = +e.target.innerText,
+                soundtracks_count = library_controls[i].innerText + 0,
 
-                          //here is need to collapse 2 loop's in one
-                for (let i = +tracks_count - 10; i < +tracks_count; i++) {
-                    tracks_array.push(tracks_base[i]);
+
+                for (let i = +soundtracks_count - 10; i < +soundtracks_count; i++) {
+                    soundtracks_array.push(soundtracks_base[i]);
                 }
 
-                for (let i = 0; i < tracks_array.length; i++) {
-                    tracks_array[i] === undefined ? library_titles[i].innerHTML = "" : library_titles[i].innerHTML = tracks_array[i].trackName;
-                    if (tracks_array[i] === undefined) {
-                        library_audio[i].removeAttribute("controls");
+            for (let i = 0; i < soundtracks_array.length; i++) {
+                soundtracks_array[i] === undefined ? library_titles[i].innerHTML = "" : library_titles[i].innerHTML = soundtracks_array[i].trackName;
+                if (soundtracks_array[i] === undefined) {
+                    library_audio[i].removeAttribute("controls");
+                } else {
+                    if (library_audio[i].hasAttribute("controls")) {
+                        library_audio[i].setAttribute("src", soundtracks_array[i].src);
                     } else {
-                        if (library_audio[i].hasAttribute("controls")) {
-                            library_audio[i].setAttribute("src", tracks_array[i].src);
-                        } else {
-                            library_audio[i].setAttribute("controls", "controls");
-                            library_audio[i].setAttribute("src", tracks_array[i].src);
-                        }
-
-
+                        library_audio[i].setAttribute("controls", "controls");
+                        library_audio[i].setAttribute("src", soundtracks_array[i].src);
                     }
-                }
 
-                //it over here
-                if(Math.ceil(tracks_base.length / 10) - curent_page === 2 || curent_page === Math.ceil(tracks_base.length / 10)) {
-                    library_controls[3].innerHTML = Math.ceil(tracks_base.length / 10) - 1;
+
+                }
+            }
+
+            if (Math.ceil(soundtracks_base.length / 10) - curent_page === 1 || curent_page === Math.ceil(soundtracks_base.length / 10)) {
+
+                for (let i = 0; i < library_controls.length; i++) {
+                    library_controls[i].className = "library__bottom-controlsItem";
+                }
+                library_controls[i].className += "-active";
+            } else {
+                if (curent_page >= 3) {
                     for (let i = 0; i < library_controls.length; i++) {
                         library_controls[i].className = "library__bottom-controlsItem";
                     }
-                    library_controls[i].className += "-active";
+                    library_controls[0].innerText = curent_page - 2;
+                    library_controls[1].innerText = curent_page - 1;
+                    library_controls[2].className += "-active";
+                    library_controls[2].innerText = curent_page;
+                    library_controls[3].innerText = curent_page + 1;
+                    library_controls[4].innerText = curent_page + 2;
                 } else {
-                    if (curent_page >= 2) {
-                        for (let i = 0; i < library_controls.length; i++) {
-                            library_controls[i].className = "library__bottom-controlsItem";
-                        }
-                        library_controls[0].innerText = curent_page - 1;
-                        library_controls[1].innerText = curent_page;
-                        library_controls[1].className += "-active";
-                        library_controls[2].innerText = curent_page + 1;
-                    } else {
-                        for (let i = 0; i < library_controls.length; i++) {
-                            library_controls[i].className = "library__bottom-controlsItem";
-                        }
-                        e.target.className += "-active"
-    
+                    for (let i = 0; i < library_controls.length; i++) {
+                        library_controls[i].className = "library__bottom-controlsItem";
                     }
-                }
+                    e.target.className += "-active"
 
+                }
             }
+
         }
+    }
+
 
 }
